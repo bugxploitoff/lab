a51078f321eed79ba1694dbf2809087f8f9c8492
Sumon Nath <snath2973@gmail.com>
2024-02-03 17:39:16 +0530

    Update mahi.jsx

--- pages/labs/mahi.jsx
+++ pages/labs/mahi.jsx
@@ -1,83 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { withIronSession } from "next-iron-session";
+import React from 'react'
 
-const ProtectedPage = ({ user }) => {
-  const [data, setData] = useState({});
+const test = () => {
+  return (
+    <div>test</div>
+  )
+}
 
-  const role = user?.user;
-
-  useEffect(() => {
-    if (user && role === "admin") {
-      const createIssue = async () => {
-        try {
-          const response = await fetch(
-            "https://bugxploit.s3.ap-south-1.amazonaws.com/images/overusage/create.json"
-          );
-          const data = await response.json();
-          setData(data);
-
-          const response1 = await fetch('/api/email', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email: data.email }),
-          });
-        } catch (error) {
-          console.error("Error in createIssue:", error);
-        }
-      };
-
-      // Call createIssue when the component mounts (on page load)
-      createIssue();
-    }
-  }, [user, role]);
-
-  if (user) {
-    if (role === "admin") {
-      return (
-        <div>
-          <div>
-            You hurt me now I am vulnerable because you {data.message}
-          </div>
-        </div>
-      );
-    } else {
-      return <div>you are mahi but not vulnerable</div>;
-    }
-  }
-
-  return <h1>You are not mahi</h1>;
-};
-
-export const getServerSideProps = withIronSession(
-  async ({ req, res }) => {
-    try {
-      const user = req.session.get("user");
-
-      if (!user) {
-        res.statusCode = 404;
-        res.end();
-        return { props: {} };
-      }
-
-      return {
-        props: { user },
-      };
-    } catch (error) {
-      console.error("Error fetching user session:", error);
-      res.statusCode = 500;
-      res.end();
-      return { props: {} };
-    }
-  },
-  {
-    cookieName: "session",
-    cookieOptions: {
-      secure: process.env.NODE_ENV === "production",
-    },
-    password: process.env.NEXT_PUBLIC_SECRET_COOKIE_PASSWORD,
-  }
-);
-
-export default ProtectedPage;
+export default test
