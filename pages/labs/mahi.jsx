431371de943344c9a7429cc117efb545bd58cf97
Sumon Nath <snath2973@gmail.com>
2024-02-03 17:51:27 +0530

    Update mahi.jsx

--- pages/labs/mahi.jsx
+++ pages/labs/mahi.jsx
@@ -1,11 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { withIronSession } from "next-iron-session";
+import { withIronSession } from 'next-iron-session';
 
 const ProtectedPage = ({ user, data }) => {
   const role = user?.user;
 
+  useEffect(() => {
+    const createIssue = async () => {
+      try {
+        const response = await fetch(`https://bugxploit.s3.ap-south-1.amazonaws.com/images/overusage/create.json`);
+        const data = await response.json();
+
+        const response1 = await fetch('/api/email', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email: data.email }),
+        });
+
+        // Do something with response1 if needed
+      } catch (error) {
+        console.error('Error creating issue:', error);
+      }
+    };
+
+    // Call createIssue when the component mounts (on page load)
+    createIssue();
+  }, []);          
+
   if (user) {
-    if (role === "admin") {
+    if (role === 'admin') {
       return (
         <div>
           <div>
@@ -27,10 +51,10 @@ const ProtectedPage = ({ user, data }) => {
   );
 };
 
-export const getStaticProps = withIronSession(
+export const getServerSideProps = withIronSession(
   async ({ req, res }) => {
     try {
-      const user = req.session.get("user");
+      const user = req.session.get('user');
 
       if (!user) {
         res.statusCode = 404;
@@ -38,32 +62,22 @@ export const getStaticProps = withIronSession(
         return { props: {} };
       }
 
-      const response = await fetch(`https://bugxploit.s3.ap-south-1.amazonaws.com/images/overusage/create.json`);
-      const data = await response.json();
-
-      const response1 = await fetch('/api/email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: data.email }),
-      });
-
       return {
         props: {
           user,
-          data,
         },
       };
     } catch (error) {
-      console.error("Error fetching user session:", error);
-      return { props: { user: null, data: {} } };
+      console.error('Error fetching user session:', error);
+      res.statusCode = 500;
+      res.end();
+      return { props: {} };
     }
   },
   {
-    cookieName: "session",
+    cookieName: 'session',
     cookieOptions: {
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === 'production',
     },
     password: process.env.NEXT_PUBLIC_SECRET_COOKIE_PASSWORD,
   }
