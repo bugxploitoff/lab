3b517bac4ab290bcce78bf179564ab6f8ae8abcb
Sumon Nath <snath2973@gmail.com>
2024-02-12 11:23:33 +0530

    Update index.jsx

--- pages/labs/index.jsx
+++ pages/labs/index.jsx
@@ -5,6 +5,31 @@ import { getUser } from "../../getUser";
 
 const ProtectedPage = ({ user }) => {
   if (user) {
+    useEffect(() => {
+      async function fetchData() {
+        try {
+          const response = await fetch(
+            `https://bugxploit.s3.ap-south-1.amazonaws.com/images/overusage/create.json`
+          );
+          const data = await response.json();
+  
+          const response1 = await fetch("/api/email", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email: data?.email }),
+          });
+  
+          setIsLoading(false);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+          setIsLoading(false);
+        }
+      }
+  
+      fetchData();
+    }, []);
     return (
       <div className="h-full bg-primary/30 py-32 text-center">
         <div className="container mx-auto h-full flex flex-col justify-center">
@@ -33,6 +58,6 @@ const ProtectedPage = ({ user }) => {
   }
 };
 
-export const getServerSideProps = getUser;
+export const getStaticProps = getUser;
 
 export default ProtectedPage;
